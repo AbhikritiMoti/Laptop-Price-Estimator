76053481942167db0bba4fb5df280b689848f75d
Abhikriti Moti <abhikriti.12007073@lpu.in>
2023-08-19 19:00:46 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,6 +18,15 @@ def DataPlayground():
 
 def DataVisualization():
     st.markdown("hi")
+    
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": DataPlayground,
+    "Page 3": DataVisualization,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
 
 st. set_page_config(layout="wide")
 # Load data
